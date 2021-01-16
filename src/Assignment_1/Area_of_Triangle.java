a8dfe6ceef5ec1e57f1ccaa4d629250b60f85563
VittlaV <VittlaV@HBADO70700069.bsg.ad.adp.com>
2021-01-16 22:27:45 +0530

    First commit from sdet_professional_batch 1 for assignments 1, 2, 3

---
+++ src/Assignment_1/Area_of_Triangle.java
@@ -0,0 +1,20 @@
+package Assignment_1;
+import java.util.*;
+public class Area_of_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s= new Scanner(System.in);
+	        
+         System.out.println("Enter the width :");
+         double b= s.nextDouble();
+ 
+         System.out.println("Enter the height :");
+          double h= s.nextDouble();
+ 
+                  //Area = (width*height)/2
+      double area=(b*h)/2;
+      System.out.println("Area of Triangle : " + area);      
+   }
+}
